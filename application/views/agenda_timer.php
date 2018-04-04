70ea6117770bd821ec0db02a6383ff4f331bbfda
senth <senth.wallace@gmail.com>
2018-04-04 14:35:58 +0200

    Update links and names

--- application/views/agenda_timer.php
+++ application/views/agenda_timer.php
@@ -3,9 +3,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
 <h1>Mastermind Agenda Timer</h1>
 <p id="participant_order"></p>
-<p><a class="waves-effect waves-light btn" onClick="startAgenda()">Start</a>
-<a class="waves-effect waves-light btn" target="_blank" rel="noopener noreferrer" href="https://hangouts.google.com/hangouts/_/6s35zznzwvfj5hn4kdx3ulciwue">Google Hangout</a>
+<p>
+<a class="waves-effect waves-light btn" target="_blank" rel="noopener noreferrer" href="https://appear.in/thevegastriangle">Video Call</a>
+<a class="waves-effect waves-light btn" target="_blank" rel="noopener noreferrer" href="https://docs.google.com/document/d/1jIIbhS2WJPpv6apkbUr21GvIhd5onSjwjVWp_PhyHx0/edit?usp=sharing">Guidelines Doc</a>
+<a class="waves-effect waves-light btn" target="_blank" rel="noopener noreferrer" href="https://docs.google.com/spreadsheets/d/1saeeHV5i5QVfL-8Jq8HyjT10llobxmcBAYLA1uZ2z-Y/edit#gid=0">My One Thing</a>
+<a class="waves-effect waves-light btn" target="_blank" rel="noopener noreferrer" href="https://docs.google.com/spreadsheets/d/15kjMjo-TnQs1IJEH92hvG4eAjZ5YQ4Mz4jTt7JF3fis/edit?usp=sharing">Shared Resources</a>
 </p>
+<p><a class="waves-effect waves-light green btn" onClick="startAgenda()">Start</a></p>
 <div id="messages"></div>
 <div class="table" id="mastermind_table"></div>
 <div id="extra_time_frame">
